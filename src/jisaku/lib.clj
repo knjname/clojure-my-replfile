27c3f05e746ba93cbd749e9f51cfa780df7f92a4
knjname <pc@knj.name>
2013-04-09 02:50:49 +0900

    add initial files

---
+++ src/jisaku/lib.clj
@@ -0,0 +1,8 @@
+(ns jisaku.lib)
+
+(print "jisaku lib loaded")
+
+(defn jisaku-func []
+  (print "jisaku-func"))
+
+
---
+++ test/cjpg/core_test.clj
@@ -0,0 +1,7 @@
+(ns cjpg.core-test
+  (:use clojure.test
+        cjpg.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
