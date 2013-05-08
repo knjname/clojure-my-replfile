b27991e7d9d41b9e5a1ce6665160a95be024bba6
knjname <pc@knj.name>
2013-05-08 22:52:52 +0900

    Decouple clojure

--- src/cjpg/core.clj
+++ src/cjpg/core.clj
@@ -537,53 +537,6 @@ java.io.File/separator ; "\\"
 (->> (a b) (c d) (e f))
 ;; 最終引数の位置に挿入されていっている
 
-;; namespace
-(in-ns 'dokodemo.door) ; #<Namespace dokodemo.door> シンボル
-(def nobita "Doraemo-n!") ; #'dokodemo.door/nobita
-
-clojure.core/refer ; OK
-refer ; java.langはインポートされるが、clojure.coreはインポートされず
-
-(in-ns 'dokodemo.door2) ; #<Namespace dokodemo.door2>
-(def nobita "Shizukacha-n!") ; #'dokodemo.door2/nobita
-nobita ; "Shizukacha-n!";;; -*-coding:utf-8-*-
-
-(in-ns 'dokodemo.door)
-nobita ; "Doraemo-n!"
-dokodemo.door2/nobita ; "Shizukacha-n!"
-
-;; create-ns は今のnamespaceを何も変更しない
-(def door3 (clojure.core/create-ns 'dokodemo.door3))
-nobita ; "Doraemo-n!" 
-door3 ; namespace object
-
-;; intern関数を使ってnamespaceオブジェクトに登録できる
-(intern door3 'suneo "Only 3 members can play this game! Nobita go home.")
-dokodemo.door3/suneo ; "Only ... home." もちろん見られる
-
-(ns-map door3) ; in-nsしたとしてそのまま見られるクラスの一覧. String -> java.lang.String みたいなキーと値の構成.
-((ns-map door3) 'String) ; java.lang.String ;  nil なら、oh みられない！ そうじゃないなら、見られる。
-
-;;; まあ、戻りましょう
-(ns cjpg.core)
-
-;; 外部から見られない変数
-(def ^:private privatething 100)
-;; そして同じように関数
-(defn- privatefunc [n] (+ n 100))
-
-privatething ; みえます
-cjpg.core/privatething ; みえます
-(privatefunc 200) ; みえます
-(cjpg.core/privatefunc 200) ; みえます
-
-(in-ns 'externalworld)
-cjpg.core/privatething ; var is not public と怒られます
-(cjpg.core/privatefunc 200) ; 同様
-
-;;; 戻ります
-(ns cjpg.core)
-
 ;; Futureを使う計算
 (import [java.util.concurrent Executors] )
 (let [ext (Executors/newCachedThreadPool)
